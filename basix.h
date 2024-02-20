6c64d1de1f3f6ee1589e102cb09161b6b1c29123
Matt Mmo <mmattmmo@gmail.com>
2024-02-20 21:41:01 +0100

    Start adding PNG file manipulation

--- basix.h
+++ basix.h
@@ -11,11 +11,14 @@
 
 #pragma once
 
+#include <bitset>
 #include <codecvt>
 #include <filesystem>
 #include <fstream>
+#include <intrin.h>
 #include <iostream>
 #include <locale>
+#include <math.h>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -77,8 +80,12 @@ namespace basix
 		return result;
 	};
 
-	// Every file type
-	enum File_Type { Text };
+	// Delete each element into an unused binary list
+	inline void delete_binary(std::vector<char*>& datas)
+	{
+		for (int i = 0; i < datas.size(); i++) delete datas[i];
+		datas.clear();
+	}
 
 	// Return the content of a directory in a vector of string.
 	inline std::vector<std::string> directory_content(std::string path) { std::vector<std::string> result; for (const auto& entry : std::filesystem::directory_iterator(path)) result.push_back(entry.path().string()); return result; };
@@ -105,6 +112,12 @@ namespace basix
 		return result;
 	}
 
+	// Returns if a path is a directory or not
+	bool path_is_directory(std::string path)
+	{
+		return ((file_datas(path).st_mode & S_IFDIR) == S_IFDIR);
+	};
+
 	// Print the "message" something in the console, coming from "sender" of type "type".
 	inline void print(std::string type, std::string sender, std::string message)
 	{
@@ -115,28 +128,46 @@ namespace basix
 	};
 
 	// Return the content of a file.
-	inline std::string read_file(std::string path, File_Type type = File_Type::Text)
+	inline std::string read_file(std::string path)
 	{
 		std::string file_content;
-		if (type == File_Type::Text)
-		{
-			std::ifstream file;
+		std::ifstream file;
 
-			// ensure ifstream objects can throw exceptions:
-			file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-			try
-			{
-				file.imbue(std::locale(std::locale::empty(), new std::codecvt_utf8<char>));
-				file.open(path);
-				std::stringstream stream; stream << file.rdbuf();
-				file.close();
-				file_content = stream.str();
-			}
-			catch (std::ifstream::failure e) { print("Error", "System", "The file \"" + path + "\" can't be opened."); }
+		// ensure ifstream objects can throw exceptions:
+		file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		try
+		{
+			file.imbue(std::locale(std::locale::empty(), new std::codecvt_utf8<char>));
+			file.open(path);
+			std::stringstream stream; stream << file.rdbuf();
+			file.close();
+			file_content = stream.str();
 		}
+		catch (std::ifstream::failure e) { print("Error", "System", "The file \"" + path + "\" can't be opened."); }
 
 		return file_content;
 	}
+
+	// Return the content of a file in binary
+	inline void read_file_binary(std::string path, std::vector<char*>& datas, std::vector<unsigned int> size, unsigned int start_pos = 0)
+	{
+		std::string file_content;
+		std::ifstream file;
+		// ensure ifstream objects can throw exceptions:
+		file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		try
+		{
+			file.open(path, std::ios::binary);
+			file.seekg(start_pos, file.beg);
+			for (int i = 0; i < datas.size(); i++)
+			{
+				file.read(datas[i], size[i]);
+			}
+			file.seekg(0, file.beg);
+			file.close();
+		}
+		catch (std::ifstream::failure e) { print("Error", "System", "The file \"" + path + "\" can't be opened, error -> " + e.what() + "."); }
+	};
 
 	// Replace a string in an another string.
 	inline std::string replace(std::string str, std::string to_replace, std::string new_str)
@@ -164,23 +195,115 @@ namespace basix
 		return std::stod(replace(str, _non_float_character, _float_character));
 	}
 
+	// Swap a buffer
+	template<typename T>
+	inline std::vector<T> swap_vector(std::vector<T> v)
+	{
+		for (int i = 0; i < floor((float)v.size() / 2.0); i++)
+		{
+			T temp = v[i];
+			v[i] = v[v.size() - (i + 1)];
+			v[i + 1] = temp;
+		}
+		return v;
+	};
+
 	// Write something in a file.
-	inline void write_in_file(std::string path, std::string to_write, std::ios::openmode opening_mode = std::ios::out, File_Type type = Text)
+	inline void write_in_file(std::string path, std::string to_write, std::ios::openmode opening_mode = std::ios::out)
 	{
 		std::ofstream file;
 		file.exceptions(std::ofstream::failbit | std::ofstream::badbit);
 		try
 		{
-			if (type == File_Type::Text)
-			{
-				file.open(path, opening_mode);
-				file << to_write;
-				file.close();
-			}
+			file.open(path, opening_mode);
+			file << to_write;
+			file.close();
 		}
 		catch (std::ofstream::failure e)
 		{
 			print("Error", "System", "The file \"" + path + "\" can't be written in error -> " + e.what() + ".");
 		}
 	}
+
+	// Write binary data in a file
+	inline void write_in_file_binary(std::string path, std::vector<char*> to_write, std::ios::openmode opening_mode = std::ios::out | std::ios::binary)
+	{
+		std::ofstream file;
+		file.exceptions(std::ofstream::failbit | std::ofstream::badbit);
+		try
+		{
+			file.open(path, opening_mode);
+			for(int i = 0;i<to_write.size();i++)
+			{
+				file.write(to_write[i], sizeof(i));
+			}
+			file.close();
+		}
+		catch (std::ofstream::failure e)
+		{
+			print("Error", "System", "The file \"" + path + "\" can't be written in error -> " + e.what() + ".");
+		}
+	}
+
+	class PNG_Image
+	{
+		// Class representing a PNG image handler
+	public:
+		// PNG_Image constructor
+		PNG_Image() {};
+		// Load the image from a path
+		inline bool load_from_path(std::string path)
+		{
+			if (file_exists(path) && !path_is_directory(path))
+			{
+				// Create the necessary things to read the PNG file
+				std::vector<char*> header = std::vector<char*>();
+				std::vector<unsigned int> size = std::vector<unsigned int>();
+				for (int i = 0; i < 8; i++)
+				{
+					header.push_back((char*)(new unsigned char(0)));
+					size.push_back(sizeof(unsigned char));
+				}
+				read_file_binary(path, header, size);
+
+				// Check if the signature is correct (137 80 78 71 13 10 26 10 for png files)
+				float signature[8];
+				signature[0] = 137;
+				signature[1] = 80;
+				signature[2] = 78;
+				signature[3] = 71;
+				signature[4] = 13;
+				signature[5] = 10;
+				signature[6] = 26;
+				signature[7] = 10;
+				for (int i = 0; i < header.size(); i++)
+				{
+					if (signature[i] != *((unsigned char*)header[i])) return false;
+				}
+				delete_binary(header);
+
+				// Check the first chunk of the file
+				// Get the size of the chunk
+				size.clear();
+				header.push_back((char*)(new unsigned int(0)));
+				size.push_back(sizeof(4));
+				read_file_binary(path, header, size, 8);
+				unsigned int chunk_size = *((int*)header[0]);
+				chunk_size = _byteswap_ulong(chunk_size);
+				delete_binary(header);
+			}
+			else
+			{
+				return false;
+			}
+		};
+		// PNG_Image destructor
+		~PNG_Image() {};
+
+		// Getters and setters
+		inline std::string get_path() { return a_path; };
+	private:
+		// Path of the image
+		std::string a_path = "";
+	};
 }
--- test.cpp
+++ test.cpp
@@ -2,6 +2,9 @@
 
 int main()
 {
+	basix::PNG_Image image;
+	image.load_from_path("home.png");
+
 	basix::print("Debug", "Developer", basix::replace("Hello world !", "o", "a"));
 
 	return 0;
