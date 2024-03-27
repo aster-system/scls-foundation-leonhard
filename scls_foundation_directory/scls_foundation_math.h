881aa9006ae782949060ac5df93b096bbc33623a
Matt Mmo <mmattmmo@gmail.com>
2024-03-27 09:44:04 +0100

    Finish Math code and documentation

--- scls_foundation_directory/scls_foundation_math.h
+++ scls_foundation_directory/scls_foundation_math.h
@@ -20,29 +20,34 @@
 // The namespace "scls" is used to simplify the all.
 namespace scls
 {
+    //*********
+	//
+	// Mathematical functions
+	//
+	//*********
+
 	// Apply the Paeth function to a left, above and upper left values
-	inline float paeth_function(float left, float above, float upper_left)
-	{
-		float p = left + above - upper_left;
-		float pa = abs(p - left);
-		float pb = abs(p - above);
-		float pc = abs(p - upper_left);
+	inline double paeth_function(double left, double above, double upper_left) {
+		double p = left + above - upper_left;
+		double pa = abs(p - left);
+		double pb = abs(p - above);
+		double pc = abs(p - upper_left);
 		if (pa <= pb && pa <= pc) return left;
 		else if (pb <= pc) return above;
-		else return upper_left;
+		return upper_left;
 	}
+
+	//*********
+	//
+	// Get mathematicals datas
+	//
+	//*********
 
 	// Normalize a value between "min" and "max" included
-	inline double normalize_value(double number, double min, double max)
-	{
-		double to_add = (max - min) + 1;
-		while (number < min) number += to_add;
-		while (number > max) number -= to_add;
-		return number;
-	}
+	inline double normalize_value(double number, double min, double max) { if(max == min) return 0; else if(max < min) std::swap(min, max); double t = (max - min); while (number < min) number += t; while (number >= max) number -= t; return number; }
 
 	// Return the size of a number
-	inline int sign(float number) { return number < 0 ? -1 : (number == 0 ? 0 : 1); };
+	inline int sign(double number) { return number < 0 ? -1 : (number == 0 ? 0 : 1); };
 }
 
 #endif // SCLS_FOUNDATION_MATH
--- test.cpp
+++ test.cpp
@@ -208,7 +208,26 @@ void test_file() {
     scls::print("Test", "SCLS File", scls::read_file("test.txt"));
 }
 
+// Test scls_foundation_math.h
+void test_math() {
+    scls::print("Test", "SCLS Math", scls::normalize_value(145, 0, 256));
+    scls::print("Test", "SCLS Math", scls::normalize_value(256 + 145, 0, 256));
+    scls::print("Test", "SCLS Math", scls::normalize_value(256 * 1547 + 145, 0, 256));
+    scls::print("Test", "SCLS Math", scls::normalize_value(-111, 0, 256));
+
+    scls::print("Test", "SCLS Math", scls::normalize_value(-111, 256, 0));
+    scls::print("Test", "SCLS Math", scls::normalize_value(-111, 256, 256));
+
+    scls::print("Test", "SCLS Math", scls::normalize_value(-129, -128, 128));
+    scls::print("Test", "SCLS Math", scls::normalize_value(128, -128, 128));
+
+    scls::print("Test", "SCLS Math", scls::sign(1546754));
+    scls::print("Test", "SCLS Math", scls::sign(-7647597));
+}
+
 int main()
 {
+    test_math();
+
     return 0;
 }
