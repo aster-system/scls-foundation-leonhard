056e243713bb8284a9f88f0df5e9256a885d8f54
Matt Mmo <mmattmmo@gmail.com>
2024-10-10 18:34:31 +0200

    Enhanced debugging system

--- scls_foundation_directory/scls_foundation_core.h
+++ scls_foundation_directory/scls_foundation_core.h
@@ -45,12 +45,15 @@
 #endif // defined
 
 #ifndef SCLS_FOUNDATION_CORE_INIT
-#define SCLS_FOUNDATION_CORE_INIT bool scls::_can_print = true;
+#define SCLS_FOUNDATION_CORE_INIT bool scls::__can_print = true;\
+    std::vector<std::string> scls::__authorized_sender = std::vector<std::string>();\
+    std::vector<std::string> scls::__authorized_type = std::vector<std::string>();\
+    std::string scls::__sender_message_separation = " : ";\
+    std::string scls::__type_sender_separation = " -> ";
 #endif // SCLS_FOUNDATION_CORE_INIT
 
 // The namespace "scls" is used to simplify the all.
-namespace scls
-{
+namespace scls {
     //*********
 	//
 	// Debugging helper
@@ -58,26 +61,24 @@ namespace scls
 	//*********
 
 	// Static vector of each authorized sender to be printed
-	static std::vector<std::string> _authorized_sender = std::vector<std::string>();
+	extern std::vector<std::string> __authorized_sender;
 	// Static vector of each authorized type to be printed
-	static std::vector<std::string> _authorized_type = std::vector<std::string>();
-
+	extern std::vector<std::string> __authorized_type;
 	// Static bool, usefull in debugging to tell to SCLS if the "print" function is enable or not
-	extern bool _can_print;
-
+	extern bool __can_print;
 	// Static string, separation between the sender and the message in the "print" function
-	static std::string _sender_message_separation = " : ";
+	extern std::string __sender_message_separation;
 	// Static string, separation between the type and the sender in the "print" function
-	static std::string _type_sender_separation = " -> ";
+	extern std::string __type_sender_separation;
 
-	// Return a reference to "_authorized_sender"
-	inline std::vector<std::string>& authorized_sender() {return _authorized_sender;};
+	// Return a reference to "__authorized_sender"
+	inline std::vector<std::string>& authorized_sender() {return __authorized_sender;};
 
-	// Return a reference to "_authorized_type"
-	inline std::vector<std::string>& authorized_type() {return _authorized_type;};
+	// Return a reference to "__authorized_type"
+	inline std::vector<std::string>& authorized_type() {return __authorized_type;};
 
-	// Return the value of "_can_print".
-	inline bool can_print() { return _can_print; };
+	// Return the value of "__can_print".
+	inline bool can_print() { return __can_print; };
 
 	// Returns if a sender is authorized or not
 	inline bool is_sender_authorized(std::string sender_to_test) {
@@ -100,27 +101,29 @@ namespace scls
 	}
 
 	// Return the value of "_sender_message_separation"
-	inline std::string sender_message_separation () {return _sender_message_separation;};
+	inline std::string sender_message_separation () {return __sender_message_separation;};
 
-	// Change the value of the static "_can_print" variable.
-	inline void set_can_print(bool new_can_print) { _can_print = new_can_print; };
+	// Change the value of the static "__can_print" variable.
+	inline void set_can_print(bool new_can_print) { __can_print = new_can_print; };
 
 	// Change the value of the static "_sender_message_separation" variable.
-	inline void set_sender_message_separation(std::string new_sender_message_separation) { _sender_message_separation = new_sender_message_separation; };
+	inline void set_sender_message_separation(std::string new_sender_message_separation) { __sender_message_separation = new_sender_message_separation; };
 
 	// Change the value of the static "_type_sender_separation" variable.
-	inline void set_type_sender_separation(std::string new_type_sender_separation) { _type_sender_separation = new_type_sender_separation; };
+	inline void set_type_sender_separation(std::string new_type_sender_separation) { __type_sender_separation = new_type_sender_separation; };
 
 	// Return the value of "_type_sender_separation"
-	inline std::string type_sender_separation () {return _type_sender_separation;};
+	inline std::string type_sender_separation () {return __type_sender_separation;};
 
 	// Print the message in the console, coming from "sender" of type "type".
 	template <typename Type_To_Print = std::string>
 	inline void print(std::string type, std::string sender, Type_To_Print message) {
 	    if (can_print() && is_type_authorized(type) && is_sender_authorized(sender)) {
-			std::cout << type << _type_sender_separation << sender << _sender_message_separation << message << std::endl;
+			std::cout << type << __type_sender_separation << sender << __sender_message_separation << message << std::endl;
 		}
 	};
+	template <typename Type_To_Print = std::string>
+	inline void print(std::string sender, Type_To_Print message) {print<Type_To_Print>("SCLS Information", sender, message);};
 
 	//*********
 	//
